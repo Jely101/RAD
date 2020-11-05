bf15ce7f1926125b0ec9a8fc11f97b88fc891ab9
Jely101 <68959210+Jely101@users.noreply.github.com>
2020-11-05 14:08:57 +0800

    Update head.php

--- Movie_search/head.php
+++ Movie_search/head.php
@@ -6,7 +6,9 @@
     <!-- title displayed on browser tab -->
     <title>Movie Search - <?php echo $title; ?></title>    
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="HandheldFriendly" content="true">
     <link rel="stylesheet" href="website_styles.css">
 </head>
 <body>
@@ -15,8 +17,7 @@
             <div class="container-fluid">
                 <!-- enables the header elements to adapt to different page widths -->
                 <div class="header">
-                    <img src="Cinema.jpg" alt="" style="width:max-content;"> <!-- the image to be used in the header -->
-                    <div class="centered">Movie Rental</div> <!-- the text ontop of the image -->
+                    Movie Rental
                 </div>
                 <h2 class="text-center"><?php echo $page ?></h2> <!-- calls the text for the title bar -->
             </div>
